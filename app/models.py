700c017c81f43d8f3bfeaa5c4f9cf146f10541e9
Luciano Ramalho <luciano@ramalho.org>
2013-06-24 19:14:32 -0300

    primeira versao do modelo da tabela de nomes

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
@@ -7,8 +7,14 @@ Base = declarative_base()
 class Nome(Base):
     __tablename__ = 'nomes'
 
-    id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True)
     nome = Column(String, unique=True)
-    nome_normalizado = Column(String, unique=True) # nome sem acentos, todo em caixa baixa
-
+    nome_normalizado = Column(String, index=True) # nome sem acentos, todo em caixa baixa
+    nome_invertido = Column(String, index=True)   # normalizado inverido (reversed)
+    genero_masculino = Column(Float, index=True, default=0)
+    genero_feminino = Column(Float, index=True, default=0)
+    genero_ambos = Column(Float, index=True, default=0)   # nomes unisex, ex: Darcy
+    conhece_zero = Column(Integer, index=True, default=0) # pessoas que não conhecem ninguém com este nome
+    conhece_uma = Column(Integer, index=True, default=0)  # pessoas que conhecem uma pessoa com este nome
+    conhece_mais = Column(Integer, index=True, default=0) # pessoas que conhecem mais de uma pessoa com este nome
 
